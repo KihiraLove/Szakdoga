0aa9f87cf60d07c431a1f2836433eaeb9cd8b3e2
Domonkos Kertesz <d.kertesz16@gmail.com>
2022-10-20 22:33:47 +0200

    script setup, camController first half

---
+++ Assets/Scripts/CamControlPC.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CamControlPC : MonoBehaviour
+{
+    public float senX;
+    public float senY;
+
+    public Transform orientation;
+
+    private float xRotation;
+    private float yRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+    }
+}
