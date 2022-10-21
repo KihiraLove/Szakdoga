5fe3b27f5e446113791e944a16f3a098fd6dd936
Domonkos Kertesz <d.kertesz16@gmail.com>
2022-10-21 22:30:14 +0200

    CamPC setup

--- Assets/Scripts/CamControlPC.cs
+++ Assets/Scripts/CamControlPC.cs
@@ -28,5 +28,8 @@ public class CamControlPC : MonoBehaviour
 
         yRotation += mouseX;
         xRotation -= mouseY;
+        
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
--- Assets/Scripts/MoveCamWithHolder.cs
+++ Assets/Scripts/MoveCamWithHolder.cs
@@ -2,18 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CurveCalc : MonoBehaviour
+public class MoveCamWithHolder : MonoBehaviour
 {
     public Transform cameraPosition;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = cameraPosition.position;
     }
 }
